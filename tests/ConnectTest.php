2849f5a879bb5ee011268bf2b5b46237398aac4b
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-08-20 17:29:37 -0400

    testes

--- tests/ConnectTest.php
+++ tests/ConnectTest.php
@@ -12,22 +12,22 @@ use PDOException;
 #[CoversClass(Connect::class)]
 class ConnectTest extends TestCase
 {
-    // public function test_getinstance_pdo_exception()
-    // {
-    //     $config = [
-    //         'driver' => 'sqlsrv',
-    //         'host' => 'localhost',
-    //         'port' => '9999',
-    //         'dbname' => 'datalayer',
-    //         'username' => 'datalayer',
-    //         'passwd' => 'datalayer',
-    //         'options' => []
-    //     ];
+    public function test_getinstance_function_with_data_layer_config_execption()
+    {
+        $config = [
+            'driver' => 'sqlsrv',
+            'host' => 'localhost',
+            'port' => '9999',
+            'dbname' => 'datalayer',
+            'username' => 'datalayer',
+            'passwd' => 'datalayer',
+            'options' => []
+        ];
 
-    //     Connect::getInstance($config);
+        Connect::getInstance($config);
 
-    //     $this->assertInstanceOf(PDOException::class, Connect::getError());
-    // }
+        $this->assertInstanceOf(PDOException::class, Connect::getError());
+    }
 
     public function test_getinstance_function_with_data_layer_config_config()
     {
@@ -42,21 +42,6 @@ class ConnectTest extends TestCase
         ];
 
         $this->assertInstanceOf(PDO::class, Connect::getInstance($config));
-    }
-
-    public function test_getinstance_function_with_data_layer_config_const()
-    {
-        define('DATA_LAYER_CONFIG', [
-            'driver' => 'mysql',
-            'host' => 'mariadb',
-            'port' => '3306',
-            'dbname' => 'datalayer',
-            'username' => 'datalayer',
-            'passwd' => 'datalayer',
-            'options' => []
-        ]);
-
-        $this->assertInstanceOf(PDO::class, Connect::getInstance());
     }
 
     public function test_constructor()
